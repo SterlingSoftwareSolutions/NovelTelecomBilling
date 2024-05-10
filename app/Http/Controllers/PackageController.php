9518e0de7abe8c14cddde67bd371eb5b87b49616
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-05-10 15:41:03 +0530

    packageservice data navigate done

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Package;
 use App\Models\PackageOption;
 use Illuminate\Http\Request;
-
+use App\Models\Account;
+use App\Models\AccountService;
+use Exception;
+use Illuminate\Support\Facades\Log;
 class PackageController extends Controller
 {
     
@@ -18,15 +21,12 @@ class PackageController extends Controller
     // }
     public function getPackages()
     { 
-        // dd("fuck you");
-        // $packages = Package::all();
-
+        
         // return response()->json($packages);
         try {
             // Retrieve packages from the database or any other data source
             $packages = Package::all(); // Example query to retrieve all packages
             //  TODO GET SUB PACKEGES
-
             return response()->json($packages);
         } catch (\Exception $e) {
             // Log the error for further investigation
@@ -44,6 +44,43 @@ class PackageController extends Controller
 
         return response()->json($packageOptions->all());
         // return response()->json($request);
+    }
+
+    public function storeAccountService(Request $request)
+    {
+        // dd($request);
+        try {
+            $validatedData = $request->validate([
+                'contact_code' => 'required',
+                'service_id' => 'required',
+                'package_id' => 'required',
+                'packageoption_id' => 'required',
+                'network' => 'required',
+                'service_type' => 'required',
+                'phonenumber' => 'required',
+                'status' => 'required',
+                'dob' => 'required|date',
+                'password' => 'required',
+                'parent' => 'required',
+                'dealer' => 'required',
+                'costcentre' => 'required'
+            ]);
+
+            $account = Account::where('contact_code', $validatedData['contact_code'])->first();
+
+            if ($account) {
+                $accountService = new AccountService();
+                $accountService->fill($validatedData);
+                $accountService->save();
+
+                return response()->json(['message' => 'Account service created successfully'], 201);
+            } else {
+                return response()->json(['error' => 'Account not found'], 404);
+            }
+        } catch (\Exception $e) {
+            Log::error('Error creating account service: ' . $e->getMessage());
+            return response()->json(['error' => 'Internal Server Error'], 500);
+        }
     }
    
 }
