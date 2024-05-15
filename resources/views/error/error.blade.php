26213067b1656c00cb1ba551ba3db23321d33ed2
isurulekanayaka <133802550+isurulekanayaka@users.noreply.github.com>
2024-05-15 12:21:50 +0530

    error page done

--- resources/views/error/error.blade.php
+++ resources/views/error/error.blade.php
@@ -1,43 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>404 - Page Not Found</title>
-  <style>
-    body {
-      background-color: #FFF9F2;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      min-height: 100vh;
-      margin: 0;
-    }
-
-    .container {
-      text-align: center;
-    }
-
-    .button {
-      background-color: rgb(51, 65, 85);
-      border: none;
-      color: white;
-      padding: 15px 20px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      cursor: pointer;
-      border-radius: 15px;
-      margin-top: 20px;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Page Not Found</title>
+    <!-- Include the Tailwind CSS CDN link -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-  <div class="container">
-    <h1>Oops! Page Not Found</h1>
-    <p>The page you requested could not be found. It might be unavailable or removed.</p>
-    <a class="button" href="/">Go to Homepage</a>
-  </div>
+
+<body class="bg-white flex justify-center items-center min-h-screen">
+
+    <div class="text-center">
+        <h1 class="text-5xl font-bold">Oops! Page Not Found</h1>
+        <p class="mt-4 text-2xl">The page you requested could not be found. It might be unavailable or removed.</p>
+        <a href="/home"
+            class="mt-8 inline-block px-8 py-4 rounded-lg text-lg font-medium text-white bg-gray-600 hover:bg-gray-700">Go
+            to Homepage</a>
+    </div>
 </body>
+
 </html>
