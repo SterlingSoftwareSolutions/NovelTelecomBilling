45a2d390b98ddf243b2cee9c5d6b763806c203d4
sajith111 <sajithniroj@gmail.com>
2024-05-15 10:53:05 +0530

    added BG and Button Colour

--- resources/views/error/error.blade.php
+++ resources/views/error/error.blade.php
@@ -4,10 +4,40 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>404 - Page Not Found</title>
-  <link rel="stylesheet" href="404.css"> </head>
-<body>
-  <h1>Oops! Page Not Found</h1>
-  <p>The page you requested could not be found. It might be unavailable or removed.</p>
+  <style>
+    body {
+      background-color: #FFF9F2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      margin: 0;
+    }
 
+    .container {
+      text-align: center;
+    }
+
+    .button {
+      background-color: rgb(51, 65, 85);
+      border: none;
+      color: white;
+      padding: 15px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      cursor: pointer;
+      border-radius: 15px;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Oops! Page Not Found</h1>
+    <p>The page you requested could not be found. It might be unavailable or removed.</p>
+    <a class="button" href="/">Go to Homepage</a>
+  </div>
 </body>
 </html>
