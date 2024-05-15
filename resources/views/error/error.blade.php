f771cbacc8c45d1613c0562959305c0bcbe45960
sajith111 <sajithniroj@gmail.com>
2024-05-15 08:11:58 +0530

    Create Page not found

---
+++ resources/views/error/error.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 - Page Not Found</title>
+  <link rel="stylesheet" href="404.css"> </head>
+<body>
+  <h1>Oops! Page Not Found</h1>
+  <p>The page you requested could not be found. It might be unavailable or removed.</p>
+
+</body>
+</html>
