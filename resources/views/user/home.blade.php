6a2396bd89ba1f1f4f9d901575cfeb33725178d9
Sajith <sajithniroj@gmail.com>
2024-05-29 09:56:00 +0530

    Bar service Width and Height

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1335,9 +1335,8 @@
             </div>
 
 
-            <div id="Bar"
-                class="popup-container  fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 z-50 flex items-center justify-center hidden">
-                <div class="bg-white border border-gray-300 shadow-lg rounded-lg p-4 relative w-1/2 h-1/2">
+            <div id="Bar" class="popup-container fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 z-50 flex items-center justify-center hidden">
+                <div class="bg-white border border-gray-300 shadow-lg rounded-lg p-4 relative w-3/4 h-[90%]">
                     <!-- Close button -->
                     <button class="absolute top-2 right-2 text-gray-600 hover:text-gray-800" onclick="hidePopupWithId('Bar')">
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
