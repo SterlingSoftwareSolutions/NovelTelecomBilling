82f4033817cd6440f55cd12c48851596103cb13b
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-05-29 11:00:20 +0530

    updated seeders

--- database/seeders/PackagesTableSeeder.php
+++ database/seeders/PackagesTableSeeder.php
@@ -25,16 +25,18 @@ class PackagesTableSeeder extends Seeder
 
             // Define package for each service option
             $allpackages = [
-                'Optus 1300/1800' => ['Optus 1300/1800 Package', 'aaaa'],
-                'Comms Code 1300/1800' => ['Comms Code 1300/1800 Package', 'aassdsdadsa'],
-                'AAPT 13/1300' => ['AAPT 13/1300 Package'],
-                'AAPT 1800' => ['AAPT 1800 Package'],
-                'Synchronous Data' => ['Synchronous Data Package'],
-                'LBNNCo Fast Fiber' => ['LBN NCo Fast Fiber Package'],
-                'AAPT 5G Fixed Wireless' => ['AAPT 5G Fixed Wireless Package'],
-                'Opticomm Fast Fiber' => ['Opticomm Fast Fiber Package'],
-                'Fixed Wireless' => ['Fixed Wireless Package'],
-                'Telstra DSL' => ['Telstra DSL Package'],
+                'Optus 1300/1800' => ['NOVELTEL INBOUND'],
+                'Comms Code 1300/1800' => [],
+                'AAPT 13/1300' => ['NOVELTEL INBOUND'],
+                'AAPT 1800' => ['NOVELTEL INBOUND'],
+                
+                '3CX Hosted'=>['Noveltel 3CX Hosted PBX'],
+                'Telstra Mobile Symbio'=>['Novel Tel 4G DATA [40GB]','Noveltel Telstra Mobile V&D Manual'],
+                'Superloop NBN'=>['NOVEL TEL NBN DATA','NovelTel NBN Data'],
+                'AAPT NBN'=>['NOVEL TEL NBN DATA','NovelTel NBN Data'],
+                'IP Express'=>['Novel Tel SIP Basic','Novel Tel SIP Basic Plus','Novel Tel SIP Ultimate','Novel NBN Vocie Only','NOVEL TEL FIXED LINE BASIC','NOVEL TEL DSL BASIC PLUS','NovelTel NBN Voice Only-Unlimited','NovelTel NBN Bundle SIP'],
+                'PSTN'=>['NOVEL TEL NBN DATA','NOVEL TEL LOCAL FIXED LINE','NOVEL TEL NATIONAL FIXED LINE','NOVEL TEL TOTAL FIXED LINE','NOVEL TEL PLATINUM FIXED LINE','NOVEL TEL FIXED LINE BASIC','NOVEL TEL DSL BASIC PLUS','NOVEL TEL DSL NATIONAL PLUS','NOVEL TEL DSL LOCAL PLUS','NOVEL TEL DSL TOTAL PLUS','NOVEL TEL FAX DUET'],
+
             ];
 
             $packageKeys = array_keys($allpackages);
